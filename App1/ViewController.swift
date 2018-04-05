99382206c2f87bcd8d30299343fc5deb57a5a535
StuFF mc <m+git@carrascomolina.com>
2018-04-05 10:55:52 +0200

    Initial Commit

---
+++ App1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App1
+//
+//  Created by StuFF mc on 05.04.18.
+//  Copyright © 2018 certgate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
